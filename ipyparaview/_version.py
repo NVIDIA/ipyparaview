f18576f91bd152eec7d318ca2d52b94d12a7488e
Nick Leaf <nleaf@nvidia.com>
2020-04-28 13:51:00 -0700

    Bumped the python package version
    Signed-off-by: Nick Leaf <nleaf@nvidia.com>

--- ipyparaview/_version.py
+++ ipyparaview/_version.py
@@ -1,5 +1,5 @@
 # Module version
-version_info = (0, 1, 0, 'beta', 0)
+version_info = (0, 1, 2, 'beta', 0)
 
 # Module version stage suffix map
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
